270cb5fa6b0d09c624adb2ff40138b129c244645
Pr4vD4 <Pr4vD4>
2023-03-12 04:34:18 +0300

    3

--- public/assets/js/make_crossword.js
+++ public/assets/js/make_crossword.js
@@ -1,8 +1,45 @@
-$( document ).ready(function() {
-    let counter = 0
-    $('#create-field-btn').on('click', function (){
-        $('#creating-field').append('<input name="filed_item['+ counter +']" type="checkbox" class="btn-check " id="btn-check-outlined'+ counter + '" autocomplete="off">\n' +
-            '                    <label class="btn btn-outline-primary crossword-item" for="btn-check-outlined'+ counter+ '"></label>')
-        counter += 1;
+$(document).ready(function () {
+
+
+    $('#create-field-btn').on('click', function () {
+
+        let counter = 0;
+
+        $('#creating-field').empty();
+        let x = Number($('#x').val());
+        let y = Number($('#y').val());
+
+        if (x > 100 || y > 100) {
+            return;
+        }
+        $('#creating-field').append('<div class="row d-flex"></div>');
+        $('#creating-field .row:last').append('<div class="crossword-item" style="border: solid 1px white"></div>');
+        for (let x_counter = 0; x_counter < x; x_counter++){
+
+            $('#creating-field .row:last').append('<div class="crossword-item" style="border: solid 1px white">' + x_counter + '</div>');
+        }
+        for (let y_counter = 0; y_counter < y; y_counter++) {
+            $('#creating-field').append('<div class="row d-flex"></div>');
+            $('#creating-field .row:last').append('<div class="crossword-item" style="border: solid 1px white">' + y_counter + '</div>');
+            for (x_counter = 0; x_counter < x; x_counter++, counter++) {
+                $('#creating-field .row:last').append('<div class="crossword-item" id="' + counter + '"></div>\n' +
+                    '                    <input type="hidden" name="crossword-item-' + counter + '" id="crossword-item-' + counter + '" value="0">');
+            }
+        }
+        $('#creating-field').append('<input type="submit" value="Send" className="mt-2">');
     });
+
+
+    $('body').on('click', '.crossword-item', function (e) {
+
+        $(this).toggleClass('crossword-item-pressed');
+        let id = $(this).attr('id')
+        if ($('#crossword-item-' + id).val() == '0') {
+            $('#crossword-item-' + id).val('1');
+        } else {
+            $('#crossword-item-' + id).val('0')
+        }
+    });
+
+
 });
--- resources/views/crossword/create_crossword.blade.php
+++ resources/views/crossword/create_crossword.blade.php
@@ -1,25 +1,25 @@
 @extends('layout.app')
 @section('content')
-    <div class="container d-flex flex-column ">
+    <div class="d-flex flex-column ">
         <h2 class="align-self-center mb-5">Create Your crossword</h2>
-        <div class="container w-75">
-            <form action="{{ route('store_crossword') }}" method="post">
-                @csrf
-                @method('post')
+
+        <form action="{{ route('store_crossword') }}" method="post">
+            @csrf
+            @method('post')
+            <div class="container-fluid w-50">
                 <div class="input-group mb-5">
                     <span class="input-group-text">Field size</span>
-                    <input type="number" aria-label="X" class="form-control" placeholder="X" min="2" max="100" id="x">
-                    <input type="number" aria-label="Y" class="form-control" placeholder="Y" min="2" max="100" id="y">
+                    <input type="number" aria-label="X" class="form-control" placeholder="X" min="2" max="100" id="x" name="x">
+                    <input type="number" aria-label="Y" class="form-control" placeholder="Y" min="2" max="100" id="y" name="y">
                     <button class="btn btn-outline-secondary" type="button" id="create-field-btn">Create Filed</button>
                 </div>
-                <div class="container" id="creating-field">
+            </div>
+            <div class="container-fluid d-flex flex-column align-items-center" id="creating-field">
 
-                </div>
+            </div>
 
-                <input type="submit" value="Send">
-                
-            </form>
-        </div>
+
+        </form>
 
     </div>
 
